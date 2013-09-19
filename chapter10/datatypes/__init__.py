0a3c6667656f3c750fe1038b123c0c3e0f5a04a8
Micah Ferrill <mcferrill@gmail.com>
2013-09-19 15:51:07 -0500

    Added PCM datatype.

--- chapter10/datatypes/__init__.py
+++ chapter10/datatypes/__init__.py
@@ -1,9 +1,11 @@
 from base import Base
 from ethernet import Ethernet
+from pcm import PCM
 
 # a dictionary of (<hex>, <class>) pairs mapping data types to python objects
 # (default is base)
 map = {
     0x00: Base,
+    0x09: PCM,
     #0x30: Ethernet,
 }
---
+++ chapter10/datatypes/pcm.py
@@ -0,0 +1,5 @@
+from base import Base
+
+
+class PCM(Base):
+    pass
