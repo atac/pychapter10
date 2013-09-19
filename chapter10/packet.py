fcb038f1a822445f2d2f01fa6838ccd7d238d4eb
Micah Ferrill <mcferrill@gmail.com>
2013-09-19 15:44:08 -0500

    PEP 8 fixes.

--- chapter10/packet.py
+++ chapter10/packet.py
@@ -1,13 +1,13 @@
-from datatypes import map,Base
+from datatypes import Base, map
 from array import array
 import struct
 
+
 class Packet(object):
-    '''
-    A Packet object that reads the header and skips past the body and footer
+    """A Packet object that reads the header and skips past the body and footer
     (storing the packet start position for later). The data only gets loaded
     if it's requested.
-    '''
+    """
 
     # the attribute names taken from header fields
     HEADER_KEYS = (
@@ -24,31 +24,26 @@ class Packet(object):
         ('Header Checksum',              'checksum'),
     )
 
-    def __init__(self,file):
-        '''
-        Takes an open file object with its cursor at this packet.
-        '''
+    def __init__(self, file):
+        """Takes an open file object with its cursor at this packet."""
 
-        # initialize the superclass
         object.__init__(self)
-
-        # keep a reference to the original file
         self.file = file
 
-        # store the starting position of the packet within the file
+        # Store the starting position of the packet within the file.
         self.filePos = file.tell()
 
-        # read out the raw header
+        # Read out the raw header.
         header = file.read(24)
 
-        # make sure we're not reading beyond the file length
+        # Make sure we're not reading beyond the file length.
         if not header:
             raise EOFError
 
-        # unpack the header
-        values = struct.unpack('HHIIBBBBIHH',header)
+        # Unpack the header.
+        values = struct.unpack('HHIIBBBBIHH', header)
 
-        # store our header sums for the checksum
+        # Store our header sums for the checksum.
         a = array('H')
         a.fromstring(header)
         self.sums = 0
@@ -56,74 +51,64 @@ class Packet(object):
             self.sums += part
         self.sums &= 0xffff
 
-        # parse the header values into attributes
-        for i,header in enumerate(self.HEADER_KEYS):
-            setattr(self,header[1],values[i])
+        # Parse the header values into attributes.
+        for i, header in enumerate(self.HEADER_KEYS):
+            setattr(self, header[1], values[i])
 
-        # store the checksum value
+        # Store the checksum value.
         self.checksums = self.checksum
 
-        # read the secondary header (if any)
+        # Read the secondary header (if any).
         if self.flags & 0x7:
-            # read the raw secondary header
+
+            # Read the raw secondary header.
             secondary = file.read(12)
 
-            # store our sums for checking later on
+            # Store our sums for checking later on.
             a = array('H')
             a.fromstring(secondary)
             sum = 0
             for part in a[:-1]:
                 sum += part
-            #sum &= 0xffff
-            #self.sums += sum
 
             # the main attribute
-            #@todo: make this convert to an actual time object!!!
+            #@todo: make this convert to an actual time object!
             self.time = secondary[:8]
 
-            # the secondary checksum
-            #self.checksums += struct.unpack('I',secondary[-4:])[0]
-
-        #self.sums &= 0xffff
-
-        # find out what type of data we have and init the data class
-        datatype = Base #map.get(self.dataType * 0x10,Base)
+        # Find out what type of data we have and init the data class.
+        datatype = map.get(self.dataType * 0x10, Base)
         self.body = datatype(self)
 
         # just skip past any footer data
         skip = len(self) - 24 - self.dataLength
         if self.flags & 0x7:
             skip -= 12
-        file.seek(skip,1)
+        file.seek(skip, 1)
 
     def printHeader(self):
-        print '-'*50
-        for name,attr in self.HEADER_KEYS:
-            print name + str(getattr(self,attr)).rjust(50-len(name)-2,'.')
-        if self.check():
-            print '(valid)'
-        else:
-            print '(error)'
-        print '-'*50
+        print '-' * 50
+        for name, attr in self.HEADER_KEYS:
+            print name + str(getattr(self, attr)).rjust(
+                50 - len(name) - 2, '.')
+        print self.check() and '(valid)' or '(error)'
+        print '-' * 50
 
     def raw(self):
-        '''
-        Returns the entire packet raw.
-        '''
+        """Returns the entire packet raw."""
 
-        # save our original cursor position
+        # Save our original cursor position.
         oldPos = self.file.tell()
 
-        # go to the start of the packet
+        # Go to the start of the packet.
         self.file.seek(self.filePos)
 
-        # read out the correct amount of data
+        # Read out the correct amount of data.
         raw = self.file.read(self.length)
 
-        # return the cursor to its original position
+        # Return the cursor to its original position...
         self.file.seek(oldPos)
 
-        # and send back the result
+        # and send back the result.
         return raw
 
     def check(self):
