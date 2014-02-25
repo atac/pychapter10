315c237bafb1020a25fcfd6b0888657eff7f2665
Micah Ferrill <mcferrill@gmail.com>
2014-02-25 15:53:38 -0600

    Updated setup script.

--- setup.py
+++ setup.py
@@ -1,23 +1,16 @@
-'''
-The distutils setup script
-'''
 
 from distutils.core import setup
 
 setup(
-    name = 'Chapter10',
-    version = '0.1',
-    author = 'Micah Ferrill',
-    author_email = 'webmaster@blazeofglory.org',
-    url = 'http://micahsmusings.com.com/c10/',
-    download_url = 'http://micahsmusings.com/c10/download/',
-    description = 'A parser library for the Chapter 10 data format.',
-    long_description = '''Provides a parsing library as well as multiple tools
+    name='Chapter10',
+    version='0.1',
+    author='Micah Ferrill',
+    author_email='mcferrill@gmail.com',
+    description='A parser library for the IRIG 106 Chapter 10 data format.',
+    long_description='''Provides a parsing library as well as multiple tools
 for basic data reading purposes.''',
-    packages = [
+    packages=[
         'chapter10',
         'chapter10.datatypes',
-    ],
-    scripts = [
     ],
 )
