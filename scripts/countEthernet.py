b6cf95eeaa6e984a10a65ec17ecb6f959e40e172
Micah Ferrill <mcferrill@gmail.com>
2014-02-25 15:52:09 -0600

    Removing some older scripts.

--- scripts/countEthernet.py
+++
@@ -1,9 +0,0 @@
-import chapter10
-
-f = open('samples/MSN001R1.ch10','rb')
-obj = chapter10.C10(f)
-packets = 0
-for i,packet in enumerate(obj.packets):
-    if isinstance(packet.body,chapter10.datatypes.Ethernet):
-        packets += 1
-print '%i ethernet packets found' % packets
