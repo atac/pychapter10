f3798bba23e6f5148e8ef55a7c9a4bd67b07fbca
Micah Ferrill <mcferrill@gmail.com>
2014-03-06 14:41:56 -0600

    Updated tests.

--- tests/datatypes/test_base.py
+++ tests/datatypes/test_base.py
@@ -5,12 +5,14 @@ from chapter10.datatypes import base
 
 
 def test_lazy():
-    body = base.Base(Mock(data_type=0))
+    body = base.Base(Mock(data_type=0, data_length=5,
+                          file=Mock(tell=Mock(return_value=5))))
     assert body.__dict__.get('data') is None
 
 
 def test_load(monkeypatch):
     monkeypatch.setattr(base.Base, 'parse', Mock())
-    body = base.Base(Mock(data_type=0))
+    body = base.Base(Mock(data_type=0, data_length=5,
+                          file=Mock(tell=Mock(return_value=5))))
     getattr(body, 'data', None)
     assert base.Base.parse.called
--- tests/test_c10.py
+++ tests/test_c10.py
@@ -1,5 +1,6 @@
 
 from mock import Mock
+import pytest
 
 from chapter10 import c10
 
@@ -7,5 +8,16 @@ from chapter10 import c10
 def test_construct(monkeypatch):
     monkeypatch.setattr(c10, 'Packet', Mock(side_effect=EOFError,
                                             return_value=[]))
-    c10.C10(Mock())
-    assert c10.Packet.called
+    f = Mock()
+    assert c10.C10(f).file == f
+
+
+def test_next(monkeypatch):
+    monkeypatch.setattr(c10.C10, 'find_and_parse', Mock(return_value=12))
+    assert c10.C10(Mock()).next() == 12
+
+
+def test_next_stop(monkeypatch):
+    monkeypatch.setattr(c10.C10, 'find_and_parse', Mock(side_effect=EOFError))
+    with pytest.raises(StopIteration):
+        c10.C10(Mock()).next()
