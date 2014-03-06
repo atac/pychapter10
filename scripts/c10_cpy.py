749c539ed8ce442b7c4ae34a531d93b0a73272a5
Micah Ferrill <mcferrill@gmail.com>
2014-03-06 12:57:27 -0600

    Added type option.

--- scripts/c10_cpy.py
+++ scripts/c10_cpy.py
@@ -5,7 +5,9 @@
 Options:
     -e CHANNEL..., --exclude CHANNEL...  Specify channels to ignore (csv) \
 [default: ].
-    -f --force                           Overwrite existing files."""
+    -f --force                           Overwrite existing files.
+    -t TYPE, --type TYPE                 Only copy a given data type \
+[default: ]."""
 
 import os
 
@@ -16,6 +18,10 @@ from chapter10 import C10
 if __name__ == '__main__':
     args = docopt(__doc__)
 
+    # Parse types (if given) into ints.
+    types = [t.strip() for t in args['--type'].split(',') if t.strip()]
+    types = [int(t, 16) if t.startswith('0x') else int(t) for t in types]
+
     # Don't overwrite unless explicitly required.
     if os.path.exists(args['<dst>']) and not args['--force']:
         print 'dst file already exists. Use -f to overwrite.'
@@ -23,7 +29,9 @@ if __name__ == '__main__':
 
     exclude = [e.strip() for e in args['--exclude'].split(',')]
     with open(args['<dst>'], 'wb') as out:
-        for packet in C10(args['<src>']):
+        for packet in C10(args['<src>'], False):
             if str(packet.channel_id) in exclude:
+                continue
+            elif types and packet.data_type not in types:
                 continue
             out.write(packet.raw())
