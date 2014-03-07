dc9f54534662dd41dcee49991c2b259a1f1fc770
Micah Ferrill <mcferrill@gmail.com>
2014-03-07 10:15:22 -0600

    Fixed a docstring typo.

--- scripts/c10_cpy.py
+++ scripts/c10_cpy.py
@@ -3,7 +3,7 @@
 """usage: c10_cpy.py <src> <dst> [options]
 
 Options:
-    -c CHANNEL..., --channel CHANNEL...  Specify channels to include(csv).
+    -c CHANNEL..., --channel CHANNEL...  Specify channels to include (csv).
     -e CHANNEL..., --exclude CHANNEL...  Specify channels to ignore (csv).
     -t TYPE, --type TYPE  The types of data to export (csv, may be decimal or \
 hex eg: 0x40)
