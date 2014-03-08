873447fdebcb9162d1cd0e68ed8a73fd703ce6d7
Micah Ferrill <mcferrill@gmail.com>
2014-03-08 17:44:54 -0600

    Added py2exe capability.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 
 from distutils.core import setup
 
-setup(
+kwargs = dict(
     name='Chapter10',
     version='0.1',
     author='Micah Ferrill',
@@ -10,5 +10,22 @@ setup(
     packages=[
         'chapter10',
         'chapter10.datatypes',
-    ],
-)
+    ])
+
+try:
+    import py2exe
+    kwargs.update(dict(
+        console=['scripts/c10_stat.py',
+                 'scripts/c10_dump.py',
+                 'scripts/c10_copy.py'],
+        zipfile=None,
+        options=dict(
+            py2exe=dict(
+                packages=['chapter10'],
+            )
+        )
+    ))
+except ImportError:
+    pass
+
+setup(**kwargs)
