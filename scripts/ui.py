5e7bdba1f7d69d40489af4afa723e0b42e3b1ee3
Micah Ferrill <mcferrill@gmail.com>
2014-03-17 16:51:34 -0500

    Videos working with graphically designed window.

--- scripts/ui.py
+++ scripts/ui.py
@@ -1,46 +1,60 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'video.ui'
+# Form implementation generated from reading ui file 'scripts\video.ui'
 #
-# Created: Mon Mar 17 14:26:19 2014
-#      by: pyside-uic 0.2.15 running on PySide 1.2.1
+# Created: Mon Mar 17 16:41:51 2014
+#      by: PyQt4 UI code generator 4.10.4
 #
 # WARNING! All changes made in this file will be lost!
 
-from PySide import QtCore, QtGui
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
         MainWindow.resize(800, 600)
         self.centralwidget = QtGui.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.gridLayoutWidget = QtGui.QWidget(self.centralwidget)
-        self.gridLayoutWidget.setGeometry(QtCore.QRect(0, 0, 791, 251))
-        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
-        self.grid = QtGui.QGridLayout(self.gridLayoutWidget)
-        self.grid.setContentsMargins(0, 0, 0, 0)
-        self.grid.setObjectName("grid")
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
         self.groupBox = QtGui.QGroupBox(self.centralwidget)
         self.groupBox.setGeometry(QtCore.QRect(20, 360, 761, 171))
-        self.groupBox.setObjectName("groupBox")
+        self.groupBox.setObjectName(_fromUtf8("groupBox"))
         self.play_btn = QtGui.QPushButton(self.groupBox)
         self.play_btn.setGeometry(QtCore.QRect(70, 70, 75, 23))
-        self.play_btn.setObjectName("play_btn")
+        self.play_btn.setObjectName(_fromUtf8("play_btn"))
+        self.gridLayoutWidget = QtGui.QWidget(self.centralwidget)
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(0, 0, 791, 351))
+        self.gridLayoutWidget.setObjectName(_fromUtf8("gridLayoutWidget"))
+        self.grid = QtGui.QGridLayout(self.gridLayoutWidget)
+        self.grid.setMargin(0)
+        self.grid.setObjectName(_fromUtf8("grid"))
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtGui.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
-        self.menubar.setObjectName("menubar")
+        self.menubar.setObjectName(_fromUtf8("menubar"))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtGui.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "MainWindow", None, QtGui.QApplication.UnicodeUTF8))
-        self.groupBox.setTitle(QtGui.QApplication.translate("MainWindow", "Playback", None, QtGui.QApplication.UnicodeUTF8))
-        self.play_btn.setText(QtGui.QApplication.translate("MainWindow", "Play", None, QtGui.QApplication.UnicodeUTF8))
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
+        self.groupBox.setTitle(_translate("MainWindow", "Playback", None))
+        self.play_btn.setText(_translate("MainWindow", "Play", None))
 
--- scripts/video.py
+++ scripts/video.py
@@ -2,11 +2,11 @@
 import os
 import sys
 
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 from mplayer.qt4 import QPlayerView
 import mplayer
 
-#from ui import Ui_MainWindow
+from ui import Ui_MainWindow
 
 
 # Tell mplayer.py where mplayer actually is.
@@ -16,14 +16,15 @@ mplayer.Player.exec_path = os.path.join(os.path.dirname(__file__),
 SCREEN_SIZE = (640, 480)
 
 
-class Main(QtGui.QDialog):
+class Main(QtGui.QMainWindow, Ui_MainWindow):
     def __init__(self):
-        super(Main, self).__init__()
+        super(Main, self).__init__(None)
+
+        self.setupUi(self)
 
         self.setWindowTitle('CH10 Video Player')
 
-        self.layout = QtGui.QGridLayout()
-        self.setLayout(self.layout)
+        #self.grid = QtGui.QGridLayout(self.top)
 
         self.videos = []
         for path in os.listdir('tmp'):
@@ -36,23 +37,24 @@ class Main(QtGui.QDialog):
             height = SCREEN_SIZE[1]
             width = SCREEN_SIZE[0] * len(self.videos)
         self.resize(width, height)
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(0, 0, width, height))
 
     def add_video(self, path):
-        v = QPlayerView(self, ('-nosound',))
+        v = QPlayerView(self.gridLayoutWidget, ('-nosound',))
         #v.eof.connect(self.closeAllWindows)
         v.resize(*SCREEN_SIZE)
         v.player.introspect()
         v.player.loadfile(path)
-        x, y = 0, self.layout.rowCount() - 1
+        x, y = 0, self.grid.rowCount() - 1
         if y < 0:
             y = 0
-        while self.layout.itemAtPosition(x, y) is not None:
+        while self.grid.itemAtPosition(x, y) is not None:
             if x == 3:
                 x = 0
                 y += 1
                 break
             x += 1
-        self.layout.addWidget(v, x, y)
+        self.grid.addWidget(v, x, y)
         self.videos.append(v)
 
 if __name__ == '__main__':
