70473c6324ba14b0e3525858381e2251ef5d715e
Micah Ferrill <mcferrill@gmail.com>
2014-03-19 13:09:56 -0500

    More setup work.

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 
+import sys
+
 from cx_Freeze import setup, Executable
 
 kwargs = dict(
@@ -6,10 +8,14 @@ kwargs = dict(
     version='0.1',
     author='Micah Ferrill',
     author_email='mcferrill@gmail.com',
-    description='A parser library for the IRIG 106 Chapter 10 data format.',
+    description='A parser library and toolset for the IRIG 106 Chapter 10 \
+data format.',
     options={
         'build_exe': {
             'includes': 'atexit',
+            'excludes': ['_ctypes', '_hashlib', '_socket', '_ssl', 'bz2'],
+            'append_script_to_exe': True,
+            'optimize': 2,
             'include_files': (
                 ('scripts/mplayer.exe', 'mplayer.exe'),
             )
@@ -19,7 +25,8 @@ kwargs = dict(
         Executable('scripts/c10_stat.py', base='Console'),
         Executable('scripts/c10_dump.py', base='Console'),
         Executable('scripts/c10_copy.py', base='Console'),
-        Executable('scripts/video.py', base='Win32GUI'),
+        Executable('scripts/video.py',
+                   base=sys.platform == 'win32' and 'Win32GUI' or None),
     ],
     packages=[
         'chapter10',
