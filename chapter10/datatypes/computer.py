1d7c664605f0dc347160dd106d685785e0962417
Micah Ferrill <mcferrill@gmail.com>
2014-03-21 15:04:10 -0500

    Updated type 0 format 3.

--- chapter10/datatypes/computer.py
+++ chapter10/datatypes/computer.py
@@ -1,7 +1,8 @@
 
 from collections import OrderedDict
+import struct
 
-from .base import Base
+from .base import Base, Data
 
 
 class TMATS(object):
@@ -27,6 +28,16 @@ class TMATS(object):
                             if line[0].startswith(key)])
 
 
+class NodeIndex(Data):
+    def __init__(self, raw):
+        Data.__init__(self, 'Node Index', raw)
+        self.data_type = struct.unpack('xB', raw[:2])[0]
+        self.channel_id = struct.unpack('H', raw[2:4])[0]
+        self.offset = struct.unpack('Q', raw[4:])[0]
+
+        print self.data_type, self.channel_id, self.offset
+
+
 class Computer(Base):
     """Computer generated data (eg. TMATS setup record)."""
 
@@ -39,11 +50,17 @@ class Computer(Base):
         'it',
         'fsp',
         'iec',
-        'tmats'
+        'tmats',
+        'file_size',
+        'all',
+        'indices',
+        'root_offset',
     )
 
     def parse(self):
         Base.parse(self)
+
+        self.all, self.indices = [], []
 
         if self.format > 3:
             raise NotImplementedError(
@@ -67,3 +84,35 @@ class Computer(Base):
             self.fsp = bool(self.csdw & (1 << 30))  # File Size Present
             self.iph = bool(self.csdw & (1 << 29))  # Index IPH
             self.iec = int(self.csdw & 0xffff)      # Index Entry Count
+
+            data = self.data[:]
+
+            if self.fsp:
+                self.file_size = struct.unpack('Q', self.data[:8])[0]
+                self.data = self.data[8:]
+
+            for i in xrange(self.iec):
+                self.all.append(Data('Timestamp', data[:8]))
+                data = data[8:]
+
+                if self.iph:
+                    self.all.append(Data('IPH', data[:8]))
+                    data = data[8:]
+
+                if self.it == 0:
+                    index = Data('Root Index', data[:8])
+                    data = data[8:]
+                else:
+                    index = NodeIndex(data[:12])
+                    data = data[12:]
+                self.indices.append(index)
+                self.all.append(index)
+
+            if self.it == 0:
+                self.root_offset = struct.unpack('Q', self.data[:8])[0]
+
+    def __iter__(self):
+        return iter(self.all)
+
+    def __len__(self):
+        return len(self.all)
