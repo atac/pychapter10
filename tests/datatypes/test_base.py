b98c527f328a03952127d4c57983d199919c39d4
Micah Ferrill <mcferrill@gmail.com>
2014-04-02 17:50:22 -0500

    Updated tests.

--- tests/datatypes/test_base.py
+++ tests/datatypes/test_base.py
@@ -1,18 +1,27 @@
 
 from mock import Mock
+import pytest
 
 from chapter10.datatypes import base
 
 
-def test_lazy():
-    body = base.Base(Mock(data_type=0, data_length=5,
-                          file=Mock(tell=Mock(return_value=5))))
+@pytest.fixture
+def body():
+    return base.Base(Mock(data_type=0, data_length=5, file=Mock(
+        tell=Mock(return_value=5),
+        read=Mock(return_value='1234'))))
+
+
+def test_parse(body):
+    body.parse()
+    assert 'data' in body.__dict__
+
+
+def test_lazy(body):
     assert body.__dict__.get('data') is None
 
 
-def test_load(monkeypatch):
+def test_load(monkeypatch, body):
     monkeypatch.setattr(base.Base, 'parse', Mock())
-    body = base.Base(Mock(data_type=0, data_length=5,
-                          file=Mock(tell=Mock(return_value=5))))
     getattr(body, 'data', None)
     assert base.Base.parse.called
