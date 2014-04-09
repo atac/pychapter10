c9c750c690d754062702c343fcccb1b6d823cd4d
Micah Ferrill <mcferrill@gmail.com>
2014-04-09 15:44:52 -0500

    Added tests for MIL_STD-1553 data type.

---
+++ tests/datatypes/test_ms1553.py
@@ -0,0 +1,30 @@
+
+import os
+
+from mock import Mock
+import pytest
+
+from chapter10 import C10
+from chapter10.datatypes import ms1553
+
+
+SAMPLE = os.path.join(os.path.dirname(__file__), '..', 'sample.c10')
+
+
+@pytest.mark.parametrize(
+    ('data_type',), [(t,) for t in [0x18] + list(range(0x1B, 0x20))])
+def test_reserved(data_type):
+    with pytest.raises(NotImplementedError):
+        m = ms1553.MS1553(Mock(
+            file=Mock(tell=Mock(return_value=0),
+                      read=Mock(return_value='1234')),
+            data_type=data_type,
+            data_length=2))
+        m.parse()
+
+
+def test_count():
+    for packet in C10(SAMPLE):
+        if isinstance(packet.body, ms1553.MS1553):
+            break
+    assert packet.body.msg_count == len(packet.body.messages)
