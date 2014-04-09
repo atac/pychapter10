c95f6c2fc4a5b20d1150d16ec09e4d2afb9c382d
Micah Ferrill <mcferrill@gmail.com>
2014-04-09 12:42:35 -0500

    Started IEEE-1394 tests.

---
+++ tests/datatypes/test_i1394.py
@@ -0,0 +1,17 @@
+
+from mock import Mock
+import pytest
+
+from chapter10.datatypes import i1394
+
+
+@pytest.mark.parametrize(
+    ('data_type',), [(t,) for t in range(0x5A, 0x60)])
+def test_reserved(data_type):
+    with pytest.raises(NotImplementedError):
+        b = i1394.I1394(Mock(
+            file=Mock(tell=Mock(return_value=0),
+                      read=Mock(return_value='1234')),
+            data_type=data_type,
+            data_length=2))
+        b.parse()
