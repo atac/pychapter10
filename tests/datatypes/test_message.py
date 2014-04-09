0801acd65f66c79d1e9e5c3f0620173d2ae48592
Micah Ferrill <mcferrill@gmail.com>
2014-04-09 15:30:40 -0500

    Added tests for message data type.

---
+++ tests/datatypes/test_message.py
@@ -0,0 +1,29 @@
+
+import os
+
+from mock import Mock
+import pytest
+
+from chapter10 import C10
+from chapter10.datatypes import message
+
+SAMPLE = os.path.join(os.path.dirname(__file__), '..', 'sample.c10')
+
+
+@pytest.mark.parametrize(
+    ('data_type',), [(t,) for t in range(0x31, 0x38)])
+def test_reserved(data_type):
+    with pytest.raises(NotImplementedError):
+        m = message.Message(Mock(
+            file=Mock(tell=Mock(return_value=0),
+                      read=Mock(return_value='1234')),
+            data_type=data_type,
+            data_length=2))
+        m.parse()
+
+
+def test_count():
+    for packet in C10(SAMPLE):
+        if isinstance(packet.body, message.Message):
+            break
+    assert packet.body.counter == len(packet.body.messages)
