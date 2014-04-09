e829d3d2064d314aa25ca0ca5ff63f7d53653c55
Micah Ferrill <mcferrill@gmail.com>
2014-04-09 15:47:51 -0500

    Added tests for parallel data type.

---
+++ tests/datatypes/test_parallel.py
@@ -0,0 +1,17 @@
+
+from mock import Mock
+import pytest
+
+from chapter10.datatypes import parallel
+
+
+@pytest.mark.parametrize(
+    ('data_type',), [(t,) for t in range(0x61, 0x68)])
+def test_reserved(data_type):
+    with pytest.raises(NotImplementedError):
+        p = parallel.Parallel(Mock(
+            file=Mock(tell=Mock(return_value=0),
+                      read=Mock(return_value='1234')),
+            data_type=data_type,
+            data_length=2))
+        p.parse()
