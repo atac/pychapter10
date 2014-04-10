c007f4b5c54f758c464406925e148244f0b793cc
Micah Ferrill <mcferrill@gmail.com>
2014-04-10 10:17:09 -0500

    Added tests for UART data type.

---
+++ tests/datatypes/test_uart.py
@@ -0,0 +1,17 @@
+
+from mock import Mock
+import pytest
+
+from chapter10.datatypes import uart
+
+
+@pytest.mark.parametrize(
+    ('data_type',), [(t,) for t in range(0x51, 0x58)])
+def test_reserved(data_type):
+    with pytest.raises(NotImplementedError):
+        u = uart.UART(Mock(
+            file=Mock(tell=Mock(return_value=0),
+                      read=Mock(return_value='1234')),
+            data_type=data_type,
+            data_length=2))
+        u.parse()
