9708f6599558b7f6c7965a3732b6caa0fe2e251e
Micah Ferrill <mcferrill@gmail.com>
2015-10-27 15:12:47 -0700

    Reimplemented discrete data parser.

--- chapter10/datatypes/discrete.py
+++ chapter10/datatypes/discrete.py
@@ -1,39 +1,28 @@
 
-from .base import Base, Data
+from .base import IterativeBase, Item
+
+import bitstruct
 
 
-class Discrete(Base):
-    data_attrs = Base.data_attrs + (
-        'all',
-        'events',
-        'length',
-        'mode',
-    )
+class Discrete(IterativeBase):
+    data_attrs = IterativeBase.data_attrs + ('length', 'mode')
 
     def parse(self):
-        Base.parse(self)
+        IterativeBase.parse(self)
 
         if self.format != 1:
             raise NotImplementedError('Discrete data format %s is reserved!'
                                       % self.format)
 
-        self.length = int(self.csdw >> 3 & 31)
-        self.mode = int(self.csdw & 0b111)
+        self.length, self.mode = bitstruct.unpack('p24u5u3', self.csdw)
 
         data = self.data[:]
-        self.all, self.events = [], []
+        offset = 0
         for i in range(len(data) / 12):
-            iph = Data('IPH', data[:8])
-            data = data[8:]
-            self.all.append(iph)
+            iph = data[offset:offset + 8]
+            offset += 8
 
-            event = Data('Discrete data', data[:4])
-            data = data[4:]
-            self.events.append(event)
-            self.all.append(event)
-
-    def __iter__(self):
-        return iter(self.all)
-
-    def __len__(self):
-        return len(self.all)
+            data = self.data[offset:offset + 4]
+            self.all.append(Item(data, 'Discrete data',
+                                 {'iph': iph, 'ipts': iph}))
+            offset += 4
