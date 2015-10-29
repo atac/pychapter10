c1b70d5d8a3dde5a65cb7101e3e789d9ac7cf8f7
Micah Ferrill <mcferrill@gmail.com>
2015-10-28 17:26:35 -0700

    Rewrote IEEE-1394 parser.

--- chapter10/datatypes/i1394.py
+++ chapter10/datatypes/i1394.py
@@ -1,13 +1,11 @@
 
-import struct
+from bitstring import BitArray
 
-from .base import Base, Data
+from .base import IterativeBase, Item
 
 
-class I1394(Base):
-    data_attrs = Base.data_attrs + (
-        'transactions',
-        'all',
+class I1394(IterativeBase):
+    data_attrs = IterativeBase.data_attrs + (
         'pbt',
         'sy',
         'tc',
@@ -16,67 +14,58 @@ class I1394(Base):
     )
 
     def parse(self):
-        Base.parse(self)
+        IterativeBase.parse(self)
 
         if self.format > 1:
             raise NotImplementedError('IEEE-1394 format %s is reserved!'
                                       % self.format)
 
         data = self.data[:]
-        self.all, self.transactions = [], []
 
+        offset = 0
         if self.format == 0:
-            self.pbt = int(self.csdw >> 29 & 0b111)  # Packet Body Type
-            self.sy = int(self.csdw >> 25 & 0b1111)  # Synchronization Code
-            self.tc = int(self.csdw & 0xffff)        # Transaction Count
+            self.pbt = self.csdw[:-29].int    # Packet Body Type
+            self.sy = self.csdw[-28:-25].int  # Synchronization Code
+            self.tc = self.csdw[-15:].int     # Transaction Count
 
             # Bus Status
             if self.pbt == 0:
-                word = struct.unpack('I', self.data)[0]
-                self.reset = bool(word >> 31)
+                self.reset = BitArray(bytes=self.data)[31]
 
             # Data Streaming
             elif self.pbt == 1:
-                self.transactions.append(Data('IEEE-1394 Transaction',
-                                              self.data))
+                self.all.append(Item(self.data, 'IEEE-1394 Transaction'))
 
             # General Purpose
             elif self.pbt == 2:
                 length = len(data) / self.tc
 
                 for i in range(self.tc):
-                    iph = Data('Timestamp', data[:8])
-                    data = data[8:]
-                    self.all.append(iph)
+                    ipts = self.data[offset:offset + 8]
+                    offset += 8
 
-                    trans = Data('IEEE-1394 Transaction', data[:length - 8])
-                    data = data[length - 8:]
-                    self.all.append(trans)
-                    self.transactions.append(trans)
+                    data = self.data[offset:offset + length - 8]
+                    offset += (length - 8)
+                    self.all.append(Item(data, 'IEEE-1394 Transaction',
+                                         ipts=ipts))
 
         elif self.format == 1:
-            self.ipc = int(self.csdw & 0xffff)  # Intra Packet Count
+            self.ipc = self.csdw[-15:].int  # Intra Packet Count
 
-            while True:
-                try:
-                    ipt = Data('Timestamp', data[:8])
-                    data = data[8:]
-                    self.all.append(ipt)
+            for i in range(self.ipc):
+                attrs = {'ipts': self.data[offset:offset + 8]}
+                offset += 8
 
-                    iph = Data('IPH', data[:4])
-                    data = data[4:]
-                    self.all.append(iph)
+                iph = BitArray(bytes=self.data[offset:offset + 4])
+                iph.byteswap()
+                attrs.update({
+                    'status': iph[:-24].int,
+                    'speed': iph[-23:-21].int,
+                    'trfovf': iph[-19:-18].int,
+                    'lbo': iph[-17],
+                    'data_length': iph[-15:].int})
+                offset += 4
 
-                    length = struct.unpack('HH', iph.data)[1]
-                    trans = Data('IEEE-1397 Transaction', data[:length])
-                    data = data[length:]
-                    self.all.append(trans)
-                    self.transactions.append(trans)
-                except IndexError:
-                    break
-
-    def __iter__(self):
-        return iter(self.all)
-
-    def __len__(self):
-        return len(self.all)
+                data = self.data[offset:offset + iph['data_length']]
+                offset += iph['data_length']
+                self.all.append(Item(data, 'IEEE-1384 Transaction', **attrs))
