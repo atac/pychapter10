614dc164da8182188901f46966ad4e1979f1699c
Micah Ferrill <mcferrill@gmail.com>
2015-12-17 13:42:58 -0600

    Updated video data type to use raw bit manipulations.

--- chapter10/datatypes/video.py
+++ chapter10/datatypes/video.py
@@ -26,32 +26,32 @@ class Video(IterativeBase):
 
         # Channel Specific Data Word (csdw).
         if self.format == 0:
-            self.et = self.csdw[-31]          # Embedded time
-            self.iph = self.csdw[-30]         # Intra-packet header
-            self.srs = self.csdw[-29]         # SCR/RTC Sync
-            self.klv = self.csdw[-28]         # KLV Metadata
-            self.pl = self.csdw[-27:-24].int  # Payload type
-            self.ba = self.csdw[-23]          # Byte alignment
+            self.et = (self.csdw >> 31) & 0b1       # Embedded time
+            self.iph = (self.csdw >> 30) & 0b1      # Intra-packet header
+            self.srs = (self.csdw >> 29) & 0b1      # SCR/RTC Sync
+            self.klv = (self.csdw >> 28) & 0b1      # KLV Metadata
+            self.pl = int((self.csdw >> 24) & 0xf)  # Payload type
+            self.ba = (self.csdw >> 23) & 0b1       # Byte alignment
         elif self.format == 1:
-            self.klv = self.csdw[-21]          # KLV Metadata
-            self.srs = self.csdw[-20]          # SCR/RTC Sync
-            self.iph = self.csdw[-19]          # Intra-packet header
-            self.epl = self.csdw[-18:-15].int  # Encode profile & level
-            self.et = self.csdw[-14]           # Embedded time
-            self.md = self.csdw[-13]           # Bit rate mode
-            self.tp = self.csdw[-12]           # Bit stream type
-            self.pc = self.csdw[-11:].int      # Packet count
+            self.klv = (self.csdw >> 21) & 0b1       # KLV Metadata
+            self.srs = (self.csdw >> 20) & 0b1       # SCR/RTC Sync
+            self.iph = (self.csdw >> 19) & 0b1       # Intra-packet header
+            self.epl = int((self.csdw >> 15) & 0xf)  # Encode profile & level
+            self.et = (self.csdw >> 14) & 0b1        # Embedded time
+            self.md = (self.csdw >> 13) & 0b1        # Bit rate mode
+            self.tp = (self.csdw >> 12) & 0b1        # Bit stream type
+            self.pc = int(self.csdw & 0xfff)         # Packet count
         elif self.format == 2:
-            self.aet = self.csdw[-26]         # Audio Encoding Type
-            self.el = self.csdw[-25:-22].int  # Encoding Level
-            self.klv = self.csdw[-21]         # KLV metadata
-            self.srs = self.csdw[-20]         # SCR/RTC Sync
-            self.iph = self.csdw[-19]         # Intra-packet header
-            self.ep = self.csdw[-18:-15].int  # Encoding Profile
-            self.et = self.csdw[-14]          # Embedded Time
-            self.md = self.csdw[-13]          # Bit rate mode
-            self.tp = self.csdw[-12]          # Bit stream type
-            self.pc = self.csdw[-11:].int     # Packet Count
+            self.aet = (self.csdw >> 26) & 0b1      # Audio Encoding Type
+            self.el = int((self.csdw >> 22) & 0xf)  # Encoding Level
+            self.klv = (self.csdw >> 21) & 0b1      # KLV metadata
+            self.srs = (self.csdw >> 20) & 0b1      # SCR/RTC Sync
+            self.iph = (self.csdw >> 19) & 0b1      # Intra-packet header
+            self.ep = int((self.csdw >> 15) & 0xf)  # Encoding Profile
+            self.et = (self.csdw >> 14) & 0b1       # Embedded Time
+            self.md = (self.csdw >> 13) & 0b1       # Bit rate mode
+            self.tp = (self.csdw >> 12) & 0b1       # Bit stream type
+            self.pc = int(self.csdw & 0xfff)        # Packet Count
         else:
             raise NotImplementedError(
                 'Video Format %s is reserved!' % self.format)
