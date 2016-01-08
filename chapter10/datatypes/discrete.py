1928d33283c266e8e99c879b43eb3f2861dcce89
Micah Ferrill <mcferrill@gmail.com>
2016-01-08 14:55:12 -0600

    Rewrote discrete type.

--- chapter10/datatypes/discrete.py
+++ chapter10/datatypes/discrete.py
@@ -1,25 +1,20 @@
 
-from .base import IterativeBase, Item
+from .base import IterativeBase
 
 
 class Discrete(IterativeBase):
+    csdw_format = ('=I', ((
+        (None, 24),
+        ('length', 5),
+        ('mode', 3),
+    ),),)
+    item_label = 'Discrete data'
+    item_size = 4
+    iph_format = ('=Q', ('ipts',))
 
     def parse(self):
-        IterativeBase.parse(self)
-
         if self.format != 1:
             raise NotImplementedError('Discrete data format %s is reserved!'
                                       % self.format)
 
-        self.length = int((self.csdw >> 3) & 0x1f)
-        self.mode = int(self.csdw & 0b111)
-
-        offset = 0
-        for i in range(len(self.data) / 12):
-            iph = self.data[offset:offset + 8]
-            offset += 8
-
-            data = self.data[offset:offset + 4]
-            self.all.append(Item(data, 'Discrete data',
-                                 {'iph': iph, 'ipts': iph}))
-            offset += 4
+        IterativeBase.parse(self)
