f51562e0bced38586a55122bba0af46c2a01b8b5
Micah Ferrill <mcferrill@gmail.com>
2016-01-08 11:27:44 -0600

    Rewrote analog data parser.

--- chapter10/datatypes/analog.py
+++ chapter10/datatypes/analog.py
@@ -1,63 +1,57 @@
 
 import struct
 
-from .base import IterativeBase, Item
+from .base import IterativeBase
 
 
 class Analog(IterativeBase):
 
-    def _parse_csdw(self, csdw):
-        """Parses a CSDW from raw bytes and returns a dict of values."""
+    csdw_format = ('=I', ((
+        ('same', 1),     # Is the CSDW the same for all subchannels?
+        ('factor', 4),   # Sampling rate factor.
+        ('totchan', 8),  # Subchannel count.
+        ('subchan', 8),  # Subchannel ID.
+        ('length', 6),   # Sample length.
+        ('mode', 2),     # Alignment and packing mode.
+    ),),)
+    item_label = 'Analog Sample'
+    iph_format = (None, None)
 
-        return {
-            'same': bool((csdw >> 29) & 0x1),     # Is the CSDW the same for
-                                                  # all subchannels?
-            'factor': int((csdw >> 24) & 0b111),  # Sampling rate factor.
-            'totchan': int((csdw >> 16) & 0xff),  # Subchannel count.
-            'subchan': int((csdw >> 8) & 0xff),   # Subchannel ID.
-            'length': int((csdw >> 2) & 0x3f),    # Sample length.
-            'mode': int(csdw & 0b11)}             # Alignment and packing mode.
+    def parse_csdw(self):
+        fmt, structure = self.csdw_format
+        raw = struct.unpack(fmt, self.packet.file.read(4))
+        values = dict(self._dissect(raw, structure))
+        if self.subchannels == []:
+            self.__dict__.update(values)
+        self.subchannels.append(values)
 
     def parse(self):
-        IterativeBase.parse(self)
-
         if self.format != 1:
             raise NotImplementedError('Analog format %s is reserved!'
                                       % self.format)
 
         # Parse one CSDW and see how many there are.
-        subchannel = self._parse_csdw(self.csdw)
-        self.__dict__.update(subchannel)
-        self.subchannels = [subchannel]
-        count = subchannel['totchan'] or 256  # totchan: 0 = 256
+        self.subchannels = []
+        self.parse_csdw()
+        count = self.totchan or 256  # totchan: 0 = 256
 
         # Read CSDWs for subchannels if applicable.
-        if not subchannel['same']:
+        if not self.same:
             for i in range(count - 1):
-                i *= 4
-                csdw, = struct.unpack('=I', self.data[i:i+4])
-                # csdw = BitArray(bytes=self.data[i:i+4])
-                # csdw.byteswap()
-                self.subchannels.append(self._parse_csdw(csdw))
+                self.parse_csdw()
 
-        # The current offset into self.data
-        offset = i + 4
+        IterativeBase.parse_data()
 
-        # Update raw data attribute for multiple-csdw case.
-        self.data = self.data[offset:]
+    def parse_one_item(self):
+        if self.same:
+            csdw = self.subchannels[0]
+        else:
+            csdw = self.subchannels[len(self) + 1]
 
-        # Read analog samples.
-        for i in range(count):
+        # Find the sample size (in bits).
+        length = csdw['length'] or 64  # Length 0 = 64 bits
 
-            csdw = subchannel if subchannel['same'] else self.subchannels[i]
+        # Convert length to bytes (align on 16 bits first of course).
+        self.item_size = (length / 16) + (length % 16 and 1 or 0)
 
-            # Find the sample size (in bits).
-            length = csdw['length'] or 64  # Length 0 = 64 bits
-
-            # Convert length to bytes (align on 16 bits first of course).
-            length = (length / 16) + (length % 16 and 1 or 0)
-
-            self.all.append(
-                Item(self.data[offset:length], 'Analog Sample'), **csdw)
-
-            offset += length
+        IterativeBase.parse_one_item(self)
