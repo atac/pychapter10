3115f70918ba14e7ede4e3e7268cebaee74ca7e5
Micah Ferrill <mcferrill@gmail.com>
2016-01-08 21:51:43 -0600

    Rewrote ms1553 module.

--- chapter10/datatypes/ms1553.py
+++ chapter10/datatypes/ms1553.py
@@ -1,60 +1,44 @@
 
-import struct
-
-from .base import IterativeBase, Item
+from .base import IterativeBase
 
 
 class MS1553(IterativeBase):
+    item_label = 'Message'
 
     def parse(self):
-        IterativeBase.parse(self)
-
         if self.format == 0 or self.format > 2:
             raise NotImplementedError('1553 Format %s is reserved!'
                                       % self.format)
 
-        offset = 0
-
         if self.format == 1:
-            self.ttb = int(self.csdw >> 30)
-            self.msg_count = int(self.csdw & 0xffffff)
-            iph_len = 6
+            self.csdw_format = ('=I', ((
+                ('ttb', 2),
+                (None, 6),
+                ('msg_count', 24),
+            ),),)
+            self.iph_format = ('=QHHH', ('ipts', (
+                (None, 2),
+                ('bid', 1),  # Bus ID (A/B)
+                ('me', 1),   # Message Error
+                ('rr', 1),   # RT to RT Transfer
+                ('fe', 1),   # Format Error
+                ('tm', 1),   # Response Time Out
+                (None, 3),
+                ('le', 1),   # Word Count Error
+                ('se', 1),   # Sync Type Error
+                ('we', 1),   # Invalid Word Error
+                (None, 3),
+            ), 'gap', 'length'))
 
         elif self.format == 2:
-            self.msg_count = int(self.csdw)
-            iph_len = 4
+            self.csdw_format = ('=I', ('msg_count',))
 
-        for i in range(self.msg_count):
-            attrs = {'ipts': self.data[offset:offset + 8]}
-            offset += 8
-            iph = self.data[offset:offset + iph_len]
-            offset += iph_len
-            if self.format == 1:
-                gap, length = struct.unpack('=HH', iph[-4:])
-                status, = struct.unpack('=H', iph[:2])
-                attrs = {
-                    'gap': gap,
-                    'length': length,
-                    'bid': (status >> 13) & 0x1,  # Bus ID (A/B)
-                    'me': (status >> 12) & 0x1,   # Message Error
-                    'rr': (status >> 11) & 0x1,   # RT to RT Transfer
-                    'fe': (status >> 10) & 0x1,   # Format Error
-                    'tm': (status >> 9) & 0x1,    # Response Time Out
-                    'le': (status >> 5) & 0x1,    # Word Count Error
-                    'se': (status >> 4) & 0x1,    # Sync Type Error
-                    'we': (status >> 3) & 0x1,    # Invalid Word Error
-                }
-            elif self.format == 2:
-                length, status = struct.unpack('=HH', iph)
-                attrs = {
-                    'length': length,
-                    'te': (status >> 15) & 0x1,  # Transaction Error
-                    're': (status >> 14) & 0x1,  # Reset
-                    'tm': (status >> 13) & 0x1,  # Message Time Out
-                    'se': (status >> 6) & 0x1,   # Status Error
-                    'ee': (status >> 3) & 0x1,   # Echo Error
-                }
+            self.iph_format = ('=QHH', ('ipts', 'length', (
+                ('te', 1),  # Transaction Error
+                ('re', 1),  # Reset
+                ('tm', 1),  # Message Time Out
+                ('se', 1),  # Status Error
+                ('ee', 1),  # Echo Error
+            )))
 
-            data = self.data[offset:offset + attrs['length']]
-            offset += attrs['length']
-            self.all.append(Item(data, 'Message', **attrs))
+        IterativeBase.parse(self)
