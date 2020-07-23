6984761f02689c328f509d5cdaa3baa3af1120d4
Micah Ferrill <mcferrill@gmail.com>
2020-07-23 15:22:43 -0500

    Updated video format to bitstruct.

--- chapter10/datatypes/video.py
+++ chapter10/datatypes/video.py
@@ -1,4 +1,5 @@
 
+from ..util import compile_fmt
 from .base import IterativeBase
 
 
@@ -14,47 +15,45 @@ class Video(IterativeBase):
 
         # Channel Specific Data Word (csdw).
         if self._format == 0:
-            self.csdw_format = ('=I', ((
-                ('embedded_time', 1),
-                ('intra_packet_header', 1),
-                ('scr_rtc_sync', 1),
-                ('key_length_value', 1),
-                ('payload', 4),
-                ('byte_alignment', 1),
-                (None, 23),
-            ),),)
+            self.csdw_format = compile_fmt('''
+                p23
+                u1 byte_alignment
+                u4 payload
+                u1 key_length
+                u1 scr_rtc_sync
+                u1 iph
+                u1 embedded_time''')
         elif self._format == 1:
-            self.csdw_format = ('=I', ((
-                (None, 10),
-                ('key_length_value', 1),
-                ('scr_rtc_sync', 1),
-                ('intra_packet_header', 1),
-                ('encoding_profile_and_level', 4),
-                ('embedded_time', 1),
-                ('mode', 1),
-                ('type', 1),
-                ('packet_count', 12),
-            ),),)
+            self.csdw_format = compile_fmt('''
+                u12 count
+                u1 type
+                u1 mode
+                u1 embedded_time
+                u4 encoding_profile
+                u1 iph
+                u1 scr_rtc_sync
+                u1 key_length_value
+                p10''')
         elif self._format == 2:
-            self.csdw_format = ('=I', ((
-                ('audio_encoding_type', 1),  # Audio Encoding Type
-                ('encoding_level', 4),   # Encoding Level
-                ('key_length_value', 1),  # KLV metadata
-                ('scr_rtc_sync', 1),  # SCR/RTC Sync
-                ('intra_packet_header', 1),  # Intra-packet header
-                ('encoding_profile', 4),   # Encoding Profile
-                ('embedded_time', 1),   # Embedded Time
-                ('mode', 1),   # Bit rate mode
-                ('type', 1),   # Bit stream type
-                ('packet_count', 12),  # Packet Count
-            ),),)
+            self.csdw_format = compile_fmt('''
+                u12 count
+                u1 type
+                u1 mode
+                u1 embedded_time
+                u4 encoding_profile
+                u1 iph
+                u1 scr_rtc_sync
+                u1 key_length_value
+                u4 encoding_level
+                u1 audio_encoding_type
+                p5''')
         else:
             raise NotImplementedError(
                 'Video Format %s is reserved!' % self._format)
 
         self.parse_csdw()
 
-        if self.intra_packet_header:
-            self.iph_format = ('=q', ('intra_packet_timestamp',))
+        if self.iph:
+            self.iph_format = compile_fmt('u64 ipts')
 
         self.parse_data()
