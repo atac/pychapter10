5184a6c22e45fa6bf3084591e0bf0daa5f5aa1e7
Micah Ferrill <mcferrill@gmail.com>
2020-07-28 18:07:03 -0500

    Updated benchmarking script to work without tqdm (but still use it if available).

--- performance_test.py
+++ performance_test.py
@@ -3,31 +3,52 @@
 """Script to benchmark Chapter 10 parsing."""
 
 from datetime import timedelta
+import sys
 import os
 import time
 
 from chapter10 import C10
-from tqdm import tqdm
+try:
+    from tqdm import tqdm
+except ImportError:
+    tqdm = None
 
 RUNS = 100
 SOURCE_FILE = 'tests/eth.c10'
 
 file_size = os.stat(SOURCE_FILE).st_size
+total = file_size * RUNS
+bytes_to_go = total
+
+
+def show_progress(percent):
+    """Takes a percentage (as a decimal fraction) and shows progress."""
+
+    bar = ('=' * int(74 * percent)).ljust(74)
+    sys.stdout.write('[{}] {}%\r'.format(bar, round(percent * 100)))
+    sys.stdout.flush()
+
 
 if __name__ == '__main__':
     start_time = time.perf_counter()
-    with tqdm(total=file_size * RUNS,
-              unit='bytes',
-              unit_scale=True,
-              leave=False) as progress:
-        for i in range(RUNS):
-            for packet in C10(SOURCE_FILE):
+    if tqdm:
+        progress = tqdm(
+            total=total, unit='bytes', unit_scale=True, leave=False)
+    for i in range(RUNS):
+        for packet in C10(SOURCE_FILE):
+
+            if tqdm:
                 progress.update(packet.packet_length)
-                try:
-                    if len(packet):
-                        for msg in packet:
-                            bus = getattr(msg, 'bus', None)
-                except TypeError:
-                    continue
-    print('Completed in %s' %
+            else:
+                bytes_to_go -= packet.packet_length
+                percent = (total - bytes_to_go) / total
+                show_progress(percent)
+
+            try:
+                if len(packet):
+                    for msg in packet:
+                        bus = getattr(msg, 'bus', None)
+            except TypeError:
+                continue
+    print('\nCompleted in %s' %
           timedelta(seconds=time.perf_counter() - start_time))
