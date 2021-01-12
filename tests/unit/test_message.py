7880eb913592f943b4025078b0f3454cd2963313
Micah Ferrill <mcferrill@gmail.com>
2021-01-12 13:24:49 -0600

    Updated message tests to be more robust.

--- tests/unit/test_message.py
+++ tests/unit/test_message.py
@@ -1,10 +1,30 @@
 
+import pytest
+
 from chapter10 import C10, message
 from fixtures import SAMPLE
 
 
-def test_count():
+@pytest.fixture
+def packet():
     for packet in C10(SAMPLE):
         if isinstance(packet, message.MessageF0):
-            break
-    assert packet.count == len(packet)
+            return packet
+
+
+@pytest.fixture
+def msg(packet):
+    return next(packet)
+
+
+def test_csdw(packet):
+    assert packet.packet_type == 0
+    assert packet.count == 94
+
+
+def test_msg(msg):
+    assert msg.ipts == 604324042154
+    assert msg.length == 350 == len(msg.data)
+    assert msg.subchannel == 1
+    assert msg.format_error == 0
+    assert msg.data_error == 0
