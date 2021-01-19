85fc3a7661c4cda42658c8bae383775895483609
Micah Ferrill <mcferrill@gmail.com>
2021-01-19 14:23:08 -0600

    Updated analog tests.

--- tests/unit/test_analog.py
+++ tests/unit/test_analog.py
@@ -1,29 +1,32 @@
 
+import pytest
+
 from chapter10 import C10, analog
 from fixtures import ANALOG
 
 
-def test_csdw():
+@pytest.fixture
+def packet():
     for packet in C10(ANALOG):
         if isinstance(packet, analog.AnalogF1):
-            assert packet.mode == 0
-            assert packet.length == 16
-            assert packet.subchannel == 0
-            assert packet.factor == 0
-            assert packet.same == 1
+            return packet
+
+
+def test_csdw(packet):
+    assert packet.mode == 0
+    assert packet.length == 16
+    assert packet.subchannel == 0
+    assert packet.factor == 0
+    assert packet.same == 1
+
+
+def test_count(packet):
+    assert len(packet.subchannels) == packet.subchannel_count
+
+
+def test_next(packet):
+    expected = [b'\xff\xf2', b'\xdf\xed', b'e\xec']
+    for i, sample in enumerate(packet):
+        assert sample.data == expected[i]
+        if i >= 2:
             break
-
-
-def test_count():
-    for packet in C10(ANALOG):
-        if isinstance(packet, analog.AnalogF1):
-            assert len(packet.subchannels) == packet.subchannel_count
-            break
-
-
-def test_next():
-    for packet in C10(ANALOG):
-        if isinstance(packet, analog.AnalogF1):
-            for i, sample in enumerate(packet):
-                if i > 10:
-                    return
